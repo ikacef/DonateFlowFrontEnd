fbe74344649bc664bc5ffdf25661f1c1f71c88c2
2185909 <nassimelhaji@gmail.com>
2025-03-31 15:03:12 -0400

    Quelques arrangements plus quelques tests

--- src/throw/Navbar.jsx
+++ src/throw/Navbar.jsx
@@ -1,13 +1,32 @@
 import "react";
-import './navbar.css';
+import './navbar2.css';
 import logo from "../img/logoFlow.png";
+import {useNavigate} from "react-router-dom";
 
 function Navbar() {
+
+    const navigate  = useNavigate();
+
+    const navigateToHome = () => {
+        navigate('/');
+    };
+
+    const navigateToStore = () => {
+        navigate('/store');
+    };
+
+    const navigateToWallet = () => {
+        navigate('/wallet');
+    };
+
+    const navigateToCharity = () => {
+        navigate('/charity');
+    };
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navTitle navbar-brand" href="#">Home</a>
+                <a className="navTitle navbar-brand" onClick={navigateToHome}>Home</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -16,20 +35,24 @@ function Navbar() {
                 <div className="navElements navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="navElement nav-link active" aria-current="page" href="#" role="button">Charities</a>
+                            <a className="navElement nav-link active" aria-current="page" onClick={navigateToCharity}
+                               role="button">Charities</a>
                         </li>
                         <li className="nav-item">
-                            <a className=" navElement nav-link active" aria-current="page" href="#" role="button">Wallet</a>
+                            <a className=" navElement nav-link active" aria-current="page" onClick={navigateToWallet}
+                               role="button">Wallet</a>
                         </li>
                         <li className="nav-item">
-                            <a className="navElement nav-link" aria-current="page" href="#" role="button">Store</a>
+                            <a className="navElement nav-link" aria-current="page" role="button"
+                               onClick={navigateToStore}>Store</a>
                         </li>
                     </ul>
 
                     <img src={logo} className="logoFlow"></img>
 
                     <form className="d-flex" role="search">
-                        <input className="searchBar form-control me-2" type="search" placeholder="Find what you're looking for here" aria-label="Search"/>
+                        <input className="searchBar form-control me-2" type="search"
+                               placeholder="Find what you're looking for here" aria-label="Search"/>
                         <button className="searchButton " type="submit">Search</button>
                     </form>
                 </div>
